50bd6ebef7ca35e7eb3529eb63f048145cd08856
Gloria Lee <90871809+gloriaslee@users.noreply.github.com>
2021-09-29 22:54:27 -0400

    Add files via upload

---
+++ pre/02/Date.java
@@ -0,0 +1,16 @@
+public class Date{
+  public static void main(String[] args){
+    String day = "Thursday";
+    int date = 9;
+    String month = "September";
+    int year = 2021;
+    System.out.println(day);
+    System.out.println(date);
+    System.out.println(month);
+    System.out.println(year);
+    System.out.println("American Format:");
+    System.out.println(day+", "+month+" "+date+", "+year);
+    System.out.println("European Format:");
+    System.out.println(day+" "+date+" "+month+" "+year);
+  }
+}
