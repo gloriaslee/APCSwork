9e9d546dd28ee8ceaf9767cd31af50bd547ab9ca
Gloria Lee <90871809+gloriaslee@users.noreply.github.com>
2021-10-01 01:25:47 -0400

    Update Greet.java

--- 08_hellofxn/Greet.java
+++ 08_hellofxn/Greet.java
@@ -1,3 +1,27 @@
+/*
+ * Gloria Lee
+ * APCS
+ * HW08 -- Refactor Freshie Zero/Beginner Java/Copy function, hardcode, and single function call
+ *2021-09-30
+ 
+ DISCOVERIES
+ - I was prompted this while running git commit on git bash
+ Author identity unknown
+
+*** Please tell me who you are.
+
+Run
+
+  git config --global user.email "you@example.com"
+  git config --global user.name "Your Name"
+
+to set your account's default identity.
+
+UNRESOLVED QUESTIONS
+Could this hw have been done using an array of names and a for loop that goes through each name?
+ */
+
+
 public class Greet{
       public static void main(String[] args){
               greet("Louis");
