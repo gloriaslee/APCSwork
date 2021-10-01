d31867d8cafef50e88b62f83d42837bf0ea2e2dd
hon sing lee <glee20@stuy.edu>
2021-10-01 01:18:59 -0400

    edited

--- 08_hellofxn/Greet.java
+++ 08_hellofxn/Greet.java
@@ -1,5 +1,10 @@
 public class Greet{
-  public static void main(String[] args){
-    System.out.println("No hablo queso");
-  }
+      public static void main(String[] args){
+              greet("Louis");
+              greet("Elise");
+              greet("Adam");
+      }
+      public static void greet(String a){
+              System.out.println("Why, hello there, "+a+". How do you do?");
+      }
 }
