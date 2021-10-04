b198d7c079239fde88818413f2de1b6b8bce2492
Gloria Lee <90871809+gloriaslee@users.noreply.github.com>
2021-10-03 22:02:52 -0400

    Update BigSib.java

--- 09_bigsib/BigSib.java
+++ 09_bigsib/BigSib.java
@@ -1,8 +1,8 @@
 class BigSib {
   public static void main(String[] args){
     Greet BigSib = new Greet();
-    BigSib.greet("Gloria");
-    BigSib.greet("Lauren");
-    BigSib.greet("Kevin");
+    BigSib.Greet("Gloria");
+    BigSib.Greet("Lauren");
+    BigSib.Greet("Kevin");
   }
 }
