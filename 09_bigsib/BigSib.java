d096647fa4f547afe3d64d5fd94367059e990621
Gloria Lee <90871809+gloriaslee@users.noreply.github.com>
2021-10-03 21:31:20 -0400

    Create BigSib.java

---
+++ 09_bigsib/BigSib.java
@@ -0,0 +1,8 @@
+class BigSib {
+  public static void main(Strin[] args){
+    Greet BigSib = new Greet();
+    BigSib.greet("Gloria");
+    BigSib.greet("Lauren");
+    BigSib.greet("Kevin");
+  }
+}
