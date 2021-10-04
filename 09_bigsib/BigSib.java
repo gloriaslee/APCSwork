3393095896595dda6427ad8c8a55c33ef9bf3e13
Gloria Lee <90871809+gloriaslee@users.noreply.github.com>
2021-10-03 22:19:23 -0400

    Update BigSib.java

--- 09_bigsib/BigSib.java
+++ 09_bigsib/BigSib.java
@@ -1,3 +1,8 @@
+/*COOL COMPUTORS - Gloria Lee and Flounder, Lauren Lee and Bo, Kevin Li and Kirby
+APCS
+HW09 - What are BigSibs Good For/ Java Classes/How to create multiple classes
+2021-10-02
+*/
 class BigSib {
   public static void main(String[] args){
     Greet BigSib = new Greet();
