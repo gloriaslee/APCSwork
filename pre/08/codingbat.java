d744df71747d964989bba5b569c6ddd94f35a5fe
Gloria Lee <90871809+gloriaslee@users.noreply.github.com>
2021-10-04 09:19:25 -0400

    Update codingbat.java

--- pre/08/codingbat.java
+++ pre/08/codingbat.java
@@ -9,3 +9,28 @@ public double average3(int a, int b, int c){
   double average = sum/3 ;
   return average;
 }
+/*NegateOdds*/
+public int oddsAreNegated(int x){
+  if (x%2==0){
+    return x;
+  }else{
+    return -x;
+  }
+}
+/*noTeenSum*/
+public int noTeenSum(int a, int b, int c) {
+  int sum = fixTeen(a)+fixTeen(b)+fixTeen(c);
+  return sum;
+}
+public int fixTeen(int n){
+  if (n>=13 && n<=19){
+    if (n==15||n==16){
+      return n;
+    }else{
+      return 0;
+    }
+  }else{
+    return n;
+  }
+  
+}
