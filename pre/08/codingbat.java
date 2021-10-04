b6d994bd8149d2b182eede729bb17ca1eecb0d31
Gloria Lee <90871809+gloriaslee@users.noreply.github.com>
2021-10-04 09:09:39 -0400

    Create codingbat.java

---
+++ pre/08/codingbat.java
@@ -0,0 +1,11 @@
+/*Force*/
+public double force(double g, double m1, double m2, double r){
+  double force = g*m1*m2/r/r;
+  return force;
+}
+/*Average*/
+public double average3(int a, int b, int c){
+  double sum = a+b+c;
+  double average = sum/3 ;
+  return average;
+}
