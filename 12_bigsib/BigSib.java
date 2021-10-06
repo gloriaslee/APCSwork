6798f2b4068fbff04d09b00c48cf87928697bc2f
Gloria Lee <90871809+gloriaslee@users.noreply.github.com>
2021-10-06 00:19:16 -0400

    Update BigSib.java

--- 12_bigsib/BigSib.java
+++ 12_bigsib/BigSib.java
@@ -1,3 +1,10 @@
+/*
+Gloria Lee and Hamim Seam
+APCS
+HW12: On Elder Individuality and the Elimination of Radio Fuzz/Java classes and objects/Using different methods within classes
+2021-10-5
+*/
+
 class BigSib{
   public static void setHelloMsg(String a){
     System.out.print(a );
