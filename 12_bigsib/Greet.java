26f67d50385ee0a9aad9aa750eb19ba30103de9a
Gloria Lee <90871809+gloriaslee@users.noreply.github.com>
2021-10-05 23:50:31 -0400

    Update Greet.java

--- 12_bigsib/Greet.java
+++ 12_bigsib/Greet.java
@@ -1,1 +1,12 @@
-
+public class Greet{
+  public static void main(String[] args){
+    String greeting;
+    
+    BigSib richard - new BigSib();
+    rechard.setHelloMsg("Word Up");
+    
+    greeting = richard.greet("freshman");
+    System.out.println(greeting);
+    
+  } //end main()
+} //end Greet
