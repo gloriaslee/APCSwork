1923c6289a433e7a1a79b78949a5f9a3d04d6514
Gloria Lee <90871809+gloriaslee@users.noreply.github.com>
2021-10-06 22:02:01 -0400

    Update BigSib.java

--- 13_hatch/BigSib.java
+++ 13_hatch/BigSib.java
@@ -3,7 +3,7 @@ class BigSib{
   public BigSib(){
     hellomsg = "Word Up";
     System.out.println(a);
-  private static String greet(String b){
+  public String greet(String b){
     b=" "+b;
     return b;
   }
