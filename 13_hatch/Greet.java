bad192b9a1c06aafc0b793d7bad5dce606fe91d2
Gloria Lee <90871809+gloriaslee@users.noreply.github.com>
2021-10-06 22:08:54 -0400

    Update Greet.java

--- 13_hatch/Greet.java
+++ 13_hatch/Greet.java
@@ -1,7 +1,7 @@
 public class Greet{
   public static void main(String[] args){
     BigSib richard = new BigSib();
-    System.out.println(richard.greet("freshman");
+    System.out.println(richard.greet("freshman"));
     
   } //end main()
 } //end Greet
