3addcc00d0e4c253b26c292f1a71165c2053e0b9
Gloria Lee <90871809+gloriaslee@users.noreply.github.com>
2021-10-14 13:59:53 -0400

    Update BankAccount.java

--- 18_BankAccount/BankAccount.java
+++ 18_BankAccount/BankAccount.java
@@ -1,1 +1,31 @@
+/*
+Duck Duck Goose - Gloria Lee (Flounder), Jonathan Song (Pheonix), Rin Fukuoka (Tape)
+APCS
+HW18 - CPA-one
+2021-10-14
+QCC:
+DISCO:
+Lastest and Greatest Q2 Response:
+Latest and Greatest Q3 Response:
+*/
 
+public class BankAccount{
+  
+  String fullName;
+  String password;
+  short PIN;
+  int accountNumber;
+  double accountBalance;
+  
+  public BankAccount(
+  
+  public setFullName(String inputFullName){
+    fullName = inputFullName;
+  }
+  public setPassword(String inputPassword){
+    password = 
+    
+    
+    
+  public static void main(String[] args){
+    setFullname()
