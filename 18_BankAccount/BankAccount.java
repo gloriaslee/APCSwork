19020fd09e4e6bbce5ea4ceb1b6c77cf560ae1b2
Gloria Lee <90871809+gloriaslee@users.noreply.github.com>
2021-10-16 18:21:53 -0400

    Update BankAccount.java

--- 18_BankAccount/BankAccount.java
+++ 18_BankAccount/BankAccount.java
@@ -10,22 +10,48 @@ Latest and Greatest Q3 Response:
 */
 
 public class BankAccount{
-  
-  String fullName;
-  String password;
-  short PIN;
-  int accountNumber;
-  double accountBalance;
-  
-  public BankAccount(
-  
-  public setFullName(String inputFullName){
-    fullName = inputFullName;
+  static String fullName;
+  static String password;
+  static int pin;
+  static int accountNumber;
+  static double accountBalance;
+
+  public BankAccount(String a, String b, int c, int d, double f){
+    fullName = a;
+    password = b;
+    pin = c;
+    accountNumber = d;
+    accountBalance = f;
   }
-  public setPassword(String inputPassword){
-    password = 
-    
-    
-    
+
+  public static void infoPrint(){
+    System.out.println("Name: "+fullName);
+    System.out.println("Password: "+password);
+    System.out.println("PIN: "+pin);
+    System.out.println("Account Number: "+accountNumber);
+    System.out.println("Account Balance: "+accountBalance+" dollars");
+  }
+
+  public static void deposit(double depositAmount){
+    accountBalance = accountBalance + depositAmount;
+
+  }
+
+  public static void withdraw(double withdrawAmount){
+    accountBalance = accountBalance - withdrawAmount;
+
+  }
+
   public static void main(String[] args){
-    setFullname()
+    BankAccount firstAccount = new BankAccount("Flounder", "you'll never guess", 1234, 1, 10000.99);
+    infoPrint();
+    withdraw(200.49);
+    infoPrint();
+    deposit(2343.53);
+    infoPrint();
+
+  }
+
+
+}
+
