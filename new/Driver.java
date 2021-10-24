01d309d1c969a2a0a816ea2b9deb3ff8b5b78424
Gloria Lee <90871809+gloriaslee@users.noreply.github.com>
2021-10-23 23:23:04 -0400

    Update Driver.java

--- new/Driver.java
+++ new/Driver.java
@@ -1,3 +1,25 @@
+/*
+Fish and Guac
+Jacob Ng, Gloria Lee
+APCS
+HW23 -- What Does Equality Look Like?
+2021-10-23
+time spent: 1.5
+*/
+
+/*
+DISCO
+-You don't need to use .toString() to call the toString() method
+-To use Math.random and Math.round you don't need to import a library
+-Math.random will produce a double on the interval [0.0, 1.0)
+-We discovered what precond and postcond means
+-When you put something at the end of an if else statement, it will never run. It is unreachable.
+QCC
+-How were we supposed to finish in half an hour???
+-How much time did sir Gabriel spend on his hw?
+-What does equality look like?
+-
+*/
 public class Driver {
 
   public static void main( String[] args ) {
@@ -17,15 +39,16 @@ public class Driver {
 
       //test toString() methods of each Coin
       System.out.println("mine: " + mine);
-          /*===================TOP==========================
       System.out.println("yours: " + yours);
       System.out.println("wayne: " + wayne);
+
       //test flip() method
       System.out.println("\nAfter flipping...");
       yours.flip();
       wayne.flip();
       System.out.println("yours: " + yours);
       System.out.println("wayne: " + wayne);
+
       //test equals() method
       if ( yours.equals(wayne) ) {
         System.out.println( "Matchee matchee!" );
@@ -33,9 +56,9 @@ public class Driver {
       else {
         System.out.println( "No match. Firestarter you can not be." );
       }
+      /*top
       ====================BOTTOM======================*/
 
   }//end main()
 
 }//end class
-
