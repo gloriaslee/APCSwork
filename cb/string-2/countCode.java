42b87d6b6236afd2abcf1c249cc5d95330701cca
Gloria Lee <90871809+gloriaslee@users.noreply.github.com>
2021-11-07 22:03:37 -0500

    Create countCode.java

---
+++ cb/string-2/countCode.java
@@ -0,0 +1,15 @@
+public class countCode{
+  public static int countCode(String text){
+    int count = 0;
+    int length = text.length()-3;
+    for(int i = 0; i<length;i++){
+      if(text.charAt(i)=="c" && text.charAt(i+!)=="o" && text.charAt(i+3)=="e");{
+        count ++;
+      }
+    }
+    return count;
+  }
+  public static void main(String[] args){
+    System.out.println(countcode("copecodelaskfdjcowo"));
+  }
+}
