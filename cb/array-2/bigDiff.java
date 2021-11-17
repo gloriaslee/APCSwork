530ac35b5b66c197efcbe798f2307888ac23eca4
Gloria Lee <90871809+gloriaslee@users.noreply.github.com>
2021-11-17 00:25:52 -0500

    Create bigDiff.java

---
+++ cb/array-2/bigDiff.java
@@ -0,0 +1,24 @@
+/*
+Nora Miller, Gloria Lee, Ziying Jian (3 Lucky Duckies)
+ APCS
+ HW37 - PPMP[] Practicing Arrays
+ 2021-11-16
+ time spent: 1 hrs
+ 
+ QCC: the difference between [] and {}? How can we effectively layer if statements and else statements without making the code inefficient / complicated? How long should
+ each of these problems be taking us? What is the most effective way to do each problem?
+ DISCO: From piazza, you can only hardcode an array using {}. It's difficult finding the right combo of && and || if you have a lot of if and else statements 
+ layered on top of each other. We should be careful to always initialize variables like counters before using them. We have to be careful to always return something, especially 
+ when dealing with different branches of if/else statements.
+ */
+
+	public int bigDiff(int[] nums) {
+	  int max = nums[0]; 
+	  int min = nums[0];
+  
+	  for (int x : nums) {
+  		max = Math.max(max, x);
+  		min = Math.min(min, x);
+	  }
+	  return (max - min); 
+	}
