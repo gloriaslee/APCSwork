247e886cf31534e6b96e00a2b49107a90a6993f3
gloriaslee <glee20@stuy.edu>
2021-12-06 00:29:44 -0500

    final rat

--- 42_rat/Rational.java
+++ 42_rat/Rational.java
@@ -37,7 +37,7 @@ public class Rational {
     public  Rational(int a, int b){
         this();
         if (b == 0) {
-            System.err.println("Error: denominatorinator is 0. Set to 1.");
+            System.err.println("Error: denominator is 0. Set to 1.");
             numerator = a;
         } else {
             this.numerator = a;
@@ -66,13 +66,13 @@ need not reduce the fraction
 
     public void multiply(Rational a) {
         this.numerator = this.numerator * a.numerator; // this.numerator is the numerator of the instance of Rational invoking this method
-        this.denominator = this.numerator * a.denominator;
+        this.denominator = this.denominator * a.denominator;
     }
 
 
     public void divide(Rational b) {
         this.numerator = this.numerator * b.denominator;
-        this.denominator = this.numerator * b.numerator;
+        this.denominator = this.denominator * b.numerator;
     }
 
 
@@ -108,30 +108,81 @@ need not reduce the fraction
     }
 
     public void reduce(){
-      /*
-        int gcd = gcd(this.numerator, this.denominator);
-        if (this.numerator != gcd){
-          this.numerator = this.numerator / gcd;
-        }
-        if (this.denominator != gcd){
-          this.denominator = this.denominator / gcd;
-        }
 
-        */
+        int gcd = gcd(this.numerator, this.denominator);
+
+        this.numerator = this.numerator / gcd;
+        this.denominator = this.denominator / gcd;
+
     }
 
-    // public int compareTo(Rational rat){
-    //
-    // }
+    public int compareTo(Rational rat){
+
+     // should be some variant of subtraction
+     Rational diff = new Rational(this.numerator, this.denominator);
+     diff.subtract(rat);
+     if (diff.floatVal() == 0) {
+       return 0;
+     } else if(diff.floatVal()>0){
+       return 1;
+     } else {
+       return -1;
+     }
+   }
+      // since typecasting just chops off the decimal
+
+
+
+
+
 
 
     public static void main(String[] args) {
 
         Rational a = new Rational();
-        Rational q = new Rational(1,0);
         Rational r = new Rational(2, 3);
         Rational s = new Rational(1,2);
+
+
+        System.out.println( "//============testing constructors===========" );
+        System.out.println( "Default constructor: " + a + "...expecting 0/1");
+        System.out.println("Testing overwritten constructor error message:");
         Rational t = new Rational(3,0);
+        System.out.println( "t: "+ t + "...expecting 3/1");
+        System.out.println("\n");
+
+
+    /* ~~============================================================================================~~ */
+    /* ~~============================================================================================~~ */
+    /* ~~================================= PREVIOUS TEST CASES ======================================~~ */
+    /* ~~============================================================================================~~ */
+    /* ~~============================================================================================~~ */
+
+        System.out.println( "//============testing multiply/divide methods===========" );
+        System.out.println(r + "...expected 2/3");
+        System.out.println(s + "...expected 1/2");
+        System.out.println("\n");
+        r.multiply(s);
+        System.out.println("AFTER MULTIPLYING....");
+        System.out.println(r + "...expected 2/6");
+        System.out.println(s + "...expected 1/2");
+        System.out.println("\n");
+        System.out.println( "//============testing float point values===========" );
+        System.out.println("float value of r " + r.floatVal() + "...should be 0.3333");
+        System.out.println("float value of s: " + s.floatVal() + "...should be 0.5" );
+
+        r.multiply(a);
+        System.out.println("AFTER MULTIPLYING AGAIN....");
+        System.out.println(r + "...expected 0/6");
+        System.out.println(a + "...expected 0/1");
+        System.out.println("\n");
+
+        t.divide(s);
+        System.out.println("AFTER DIVIDING....");
+        System.out.println("t is: " + t + "...expecting 6/1");
+        System.out.println("s is: " + s + "...expecting 1/2");
+        System.out.println("\n");
+
 
 
         /* ~~============================================================================================~~ */
@@ -139,6 +190,12 @@ need not reduce the fraction
         /* ~~~~~~~~========================== NEW TEST CASES ===================================~~~~~~~~~~ */
         /* ~~============================================================================================~~ */
         /* ~~============================================================================================~~ */
+
+        //resetting rationals to original values
+        r = new Rational(2, 3);
+        s = new Rational(1,2);
+        t = new Rational(3,0);
+
         System.out.println( "//============testing add/subtract methods===========" );
         System.out.println("r is " + r); //
         System.out.println("s is " + s);
@@ -155,44 +212,19 @@ need not reduce the fraction
         System.out.println( s + " addition...expected 7/2" );
         s.subtract(t); //expecting 7/2 - 3/1 = 1/2
         System.out.println( s + " subtraction... expected 1/2" );
+
+        System.out.println( "//============testing reduce method===========" );
+        System.out.println("r is " + r); //
+        r.reduce();
+        System.out.println("After reduction... r = " + r + " expected 2/3");
+        System.out.println("t is " + t);
+        t.reduce();
+        System.out.println("After reduction... t = " + t + " expected 3/1");
+
+        // Testing compareTo();
+
+        System.out.println(r.compareTo(s) + " expected 1"); // expected 1
+        System.out.println(s.compareTo(r) + " expected -1"); // expected -1
+        System.out.println(r.compareTo(r) + " expected 0"); // expected 0
     }
-
-
-    /* ~~============================================================================================~~ */
-    /* ~~============================================================================================~~ */
-    /* ~~================================= PREVIOUS TEST CASES ======================================~~ */
-    /* ~~============================================================================================~~ */
-    /* ~~============================================================================================~~ */
-    // System.out.println( "//============testing constructors===========" );
-    // System.out.println( "Default constructor: " + a + "...expecting 0/1");
-    // System.out.println("Testing overwritten constructor error message:" + t + "...expecting 0/1");
-    // System.out.println("\n");
-    //
-    //
-    // System.out.println( "//============testing multiply/divide methods===========" );
-    // System.out.println(r + "...expected 2/3");
-    // System.out.println(s + "...expected 1/2");
-    // System.out.println("\n");
-    // r.multiply(s);
-    // System.out.println("AFTER MULTIPLYING....");
-    // System.out.println(r + "...expected 2/6");
-    // System.out.println(s + "...expected 1/2");
-    // System.out.println("\n");
-    // r.multiply(a);
-    // System.out.println("AFTER MULTIPLYING AGAIN....");
-    // System.out.println(r + "...expected 0/6");
-    // System.out.println(a + "...expected 0/1");
-    // System.out.println("\n");
-    //
-    //
-    // System.out.println("AFTER DIVIDING....");
-    // System.out.println("r is: " + r + "...expecting 2/6");
-    // System.out.println("s is: " + s + "...expecting 1/2");
-    // System.out.println("\n");
-    //
-    // System.out.println( "//============testing float point values===========" );
-    // System.out.println("float value of r " + r.floatVal() + "...should be 0.3333");
-    // System.out.println("float value of s: " + s.floatVal() + "...should be 0.5" );
-
-
 }//end class
