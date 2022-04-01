17cc642f698c515cf9dedd85626f146a0c165f19
Gloria Lee <glee20@stuy.edu>
2022-04-01 14:07:23 -0400

    plz

---
+++ 87_queue/NodeQueue.java
@@ -0,0 +1,28 @@
+public class Nodequeue implements Queue{
+
+  private QUASAR car;
+
+  private int _size;
+  public Nodequeue{
+
+  }
+
+  //means of removing an element from collection:
+  //Dequeues and returns the first element of the queue.
+  public QUASAR dequeue();
+
+  //means of adding an element to collection:
+  //Enqueue an element onto the back of this queue.
+  public void enqueue( QUASAR x );
+
+  //Returns true if this queue is empty, otherwise returns false.
+  public boolean isEmpty();
+
+  //Returns the first element of the queue without dequeuing it.
+  public QUASAR peekFront();
+
+  class Node
+
+
+
+}
