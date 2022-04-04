68c9f93684d6890a12b47c3d5839a8334ef0f277
Gloria Lee <glee20@stuy.edu>
2022-04-04 14:07:15 -0400

    pizza bagels

---
+++ 88_rqueue/RQueue.java
@@ -0,0 +1,140 @@
+/***
+ * class RQueue
+ * SKELETON
+ * A node-based, randomized queue
+ * (a collection with FIIDKO property)
+ *
+ *       -------------------------------
+ *   end |  --->   Q U E U E   --->    | front
+ *       -------------------------------
+ *
+ *  linkages point opposite direction for O(1) en/dequeuing
+ *            N <- N <- ... <- N <- N
+ *      _end -^                     ^- _front
+ *
+ **/
+
+
+public class RQueue<SWASHBUCKLE> implements Queue<SWASHBUCKLE>
+{
+  //instance variables
+  private LLNode _front, _end;
+  private int _size;
+
+
+  // default constructor creates an empty queue
+  public RQueue()
+  {
+    _size = 0;
+    _front = null;
+    _end = null;
+  }
+
+
+  public void enqueue( SWASHBUCKLE enQVal )
+  {
+
+
+        //special case: when enqueuing to an empty list,
+        //make _front && _end point to same node
+        if ( isEmpty() ) {
+          _front = _end = new LLNode( enQVal, null );
+        }
+        else {
+          _end.setNext( new LLNode( enQVal, null ) );
+          _end = _end.getNext();
+        }
+        _size++;
+        System.out.println("enqueued " + enQVal);
+
+
+
+
+  }//O(1)
+
+
+  // remove and return thing at front of queue
+  // assume _queue ! empty
+  public SWASHBUCKLE dequeue()
+  {
+    SWASHBUCKLE retVal = _front.getCargo();
+    _front = _front.getNext();
+
+    if ( _front == null ) //just moved past last node
+      _end = null;      //update _end to reflect emptiness
+
+    _size--;
+
+    return retVal;
+
+  }//O(?)
+
+
+  public SWASHBUCKLE peekFront()
+  {
+    return _front.getCargo();
+
+  }//O(?)
+
+
+  /***
+   * void sample() -- a means of "shuffling" the queue
+   * Algo:
+   *   < YOUR SUCCINCT SUMMARY HERE >
+   **/
+  public void sample ()
+  {
+
+  }//O(?)
+
+
+  public boolean isEmpty()
+  {
+    return _front == null;
+  } //O(?)
+
+
+  // print each node, separated by spaces
+  public String toString()
+  {
+return _cargo.toString();
+  }//end toString()
+
+
+
+  //main method for testing
+  public static void main( String[] args )
+  {
+
+      /*v~~~~~~~~~~~~~~MAKE MORE~~~~~~~~~~~~~~v
+
+    Queue<String> PirateQueue = new RQueue<String>();
+
+    System.out.println("\nnow enqueuing...");
+    PirateQueue.enqueue("Dread");
+    PirateQueue.enqueue("Pirate");
+    PirateQueue.enqueue("Roberts");
+    PirateQueue.enqueue("Blackbeard");
+    PirateQueue.enqueue("Peter");
+    PirateQueue.enqueue("Stuyvesant");
+
+    System.out.println("\nnow testing toString()...");
+    System.out.println( PirateQueue ); //for testing toString()...
+
+    System.out.println("\nnow dequeuing...");
+    System.out.println( PirateQueue.dequeue() );
+    System.out.println( PirateQueue.dequeue() );
+    System.out.println( PirateQueue.dequeue() );
+    System.out.println( PirateQueue.dequeue() );
+    System.out.println( PirateQueue.dequeue() );
+    System.out.println( PirateQueue.dequeue() );
+
+    System.out.println("\nnow dequeuing fr empty queue...\n" +
+                       "(expect NPE)\n");
+    System.out.println( PirateQueue.dequeue() );
+
+      ^~~~~~~~~~~~~~~~AWESOME~~~~~~~~~~~~~~~^*/
+
+  }//end main
+
+}//end class RQueue
