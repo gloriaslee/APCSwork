df7f48ca334941db87ac4efe5a2bd808a9ed7b30
Gloria Lee <glee20@stuy.edu>
2022-04-05 14:07:28 -0400

    wee

---
+++ cobo/100_data/Cereal.java
@@ -0,0 +1,49 @@
+public class Cereal{
+
+  private String _name;
+  private Char _type;
+  private int _cal;
+  private int _protein;
+  private int _fat;
+  private int _sodium;
+  private double _fiber;
+  private double _carbs;
+  private int _sugar;
+  private int _potas;
+  private int _vita;
+  private int _shelf;
+  private float _weight;
+  private float _cups;
+  private float _rating;
+
+
+  public Cereal(String   private String name, Char type;
+     int cal,
+     int protein,
+    private int fat,
+    private int sodium,
+    private double fiber,
+    private double carbs,
+    private int sugar,
+    private int potas,
+    private int vita,
+    private int shelf,
+    private float weight,
+    private float cups,
+    private float rating){
+    name = "";
+    type = "C";
+    cal = 0;
+    protein = 0;
+    fat = 0;
+    sodium = 0;
+
+
+  }
+
+  public String toString(){
+    return "foo";
+  }
+
+
+}
